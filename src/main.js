3ce2b71f9ff165d969a760bdeb01810f1fe493bf
Mike Harder <mharder@microsoft.com>
2019-06-10 18:45:10 -0700

    Add dependency on buffer

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import lunchtime from './lunchtime.js';
 import millisecondsUntil from './millisecondsUntil.js';
 
 export default function howLongUntilLunch(hours, minutes) {
+	const buf = Buffer.from('hello');
+	console.log(buf);
+	
 	// lunch is at 12.30
 	if (hours === undefined) hours = 12;
 	if (minutes === undefined) minutes = 30;
