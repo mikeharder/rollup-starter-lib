a555e09817c8d5ce9cfbae6ea4c3afb76611408e
Mike Harder <mharder@microsoft.com>
2019-06-17 14:13:23 -0700

    Add dependency on url

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import ms from 'ms';
 import lunchtime from './lunchtime.js';
 import millisecondsUntil from './millisecondsUntil.js';
+import * as url from "url";
 
 export default function howLongUntilLunch(hours, minutes) {
+	console.log(url.parse("http://example.com"));
+
 	// lunch is at 12.30
 	if (hours === undefined) hours = 12;
 	if (minutes === undefined) minutes = 30;
